3e2dc50ba763975a70eafbaabe56509bf8c49a27
Valdo <vteixeiraromao@gmail.com>
2022-06-30 10:57:17 +0100

    Added outline colors to staff members

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,21 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
+    if (WA.player.tags.includes('bma')) {
+        WA.player.setOutlineColor(255, 0, 255)
+    }
+    if (WA.player.tags.includes('hr')) {
+        WA.player.setOutlineColor(0, 0, 255)
+    }
+    if (WA.player.tags.includes('tech')) {
+        WA.player.setOutlineColor(255, 0, 0)
+    }
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,12 +26,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
